36a34b4b85751f6d1e11257baacab5cb37462260
Iu Ayala <iuayalaportella@gmail.com>
2022-10-29 19:56:39 +0100

    Move downloaded video to a folder

--- extract_audio.py
+++ extract_audio.py
@@ -1,5 +1,6 @@
 import subprocess
 import os.path
+from pathlib import Path
 
 
 def download_yt_video(yt_link):
@@ -9,8 +10,25 @@ def download_yt_video(yt_link):
     Link: https://github.com/yt-dlp/yt-dlp
     """
 
-    if not os.path.exists("tmp"):
-        os.mkdir("tmp")
+    # Create tmp folder to store videos and audios
+    tmp_folder = Path("tmp")
+    tmp_folder.mkdir(exist_ok=True)
 
+    # Download video
     list_files = subprocess.run(["yt-dlp", yt_link])
     print("The exit code was: %d" % list_files.returncode)
+
+    # Move video to tmp folder
+    for el in Path(".").iterdir():
+        if el.suffix == ".webm":
+            el.replace(tmp_folder / el.name)
+
+
+def extract_audio(video_path):
+    pass
+
+
+if __name__ == "__main__":
+    download_yt_video(
+        "https://www.youtube.com/watch?time_continue=128&v=2WjMcUhsMAM&feature=emb_title"
+    )
